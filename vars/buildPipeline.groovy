527dbc7613e01a9327f02d4fc047e4a0c0b8f39f
Saurav Kumar <shah.kumar.saurav@gmail.com>
2025-01-11 23:04:12 +0530

    updated logs

--- vars/buildPipeline.groovy
+++ vars/buildPipeline.groovy
@@ -10,7 +10,7 @@ def call(Map pipelineParams = [:]) {
                 steps {
                     echo "Pipeline configuration: ${PIPELINE_CONFIG}"
                     echo "Stages: ${PIPELINE_CONFIG['stages']}"  // Should print a list like [checkout, build, test]
-                    echo "Variables: ${PIPELINE_CONFIG['variables']"  // Should print a map like [environment: "dev", artifact_name: "service-a-artifact"]
+                    echo "Variables: ${PIPELINE_CONFIG['variables']}"  // Should print a map like [environment: "dev", artifact_name: "service-a-artifact"]
                     // Proceed with checkout step
                     checkout scm
                 }
