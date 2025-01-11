6915b865767cd6664811ce2e2d190a546aadc6e0
Saurav Kumar <shah.kumar.saurav@gmail.com>
2025-01-11 21:53:18 +0530

    added buildPipeline

---
+++ vars/buildPipeline.groovy
@@ -0,0 +1,41 @@
+def call(Map pipelineParams = [:]) {
+    pipeline {
+        agent any
+        environment {
+            PROJECT_NAME = pipelineParams.projectName ?: 'Default-Project'
+        }
+        stages {
+            stage('Checkout') {
+                steps {
+                    checkout scm
+                }
+            }
+            stage('Build') {
+                steps {
+                    script {
+                        echo "Building project: ${PROJECT_NAME}"
+                    }
+                }
+            }
+            stage('Test') {
+                steps {
+                    script {
+                        echo "Running tests for ${PROJECT_NAME}"
+                    }
+                }
+            }
+            stage('Deploy') {
+                steps {
+                    script {
+                        echo "Deploying ${PROJECT_NAME}"
+                    }
+                }
+            }
+        }
+        post {
+            always {
+                echo "Pipeline completed for ${PROJECT_NAME}"
+            }
+        }
+    }
+}
